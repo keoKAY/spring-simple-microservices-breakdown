53a208caece7a2d4b65c4bca73d39196da753452
KAY KEO <keokay888@gmail.com>
2025-06-03 16:51:33 +0700

    first commit

--- user-service/src/main/java/dev/sample/userservice/UserRestController.java
+++ user-service/src/main/java/dev/sample/userservice/UserRestController.java
@@ -1,15 +1,27 @@
 package dev.sample.userservice;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/users")
 public class UserRestController {
     @GetMapping("/get-all")
-    public String getAllUsers(){
-        return "Get all Users from the user-service ! ";
+    public ResponseEntity<?> getAllUsers(){
+        List<Product> response = new ArrayList<>(); 
+        response.add(Product.builder()
+                .productId(UUID.randomUUID())
+                        .productName("Product 1")
+                .productPrice(100)
+                        .productDescription("This is the product desc ription of the product 1 ")
+                .build());
+        return ResponseEntity.ok(response);
     }
 }
